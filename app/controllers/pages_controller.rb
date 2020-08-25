3d7463728ac1be123696a0ea2b0df723b1cc2d78
Victor Gervacio <victorgervacio33@gmail.com>
2020-08-25 07:12:41 -0600

    index working

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(1)
+  end
+
+  def new
+    @page = Page.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  #resources :page
+   get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
